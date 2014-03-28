fcefc1e2ffba24c2b90e0b1999088c5fd0dc8aa2
Breno Moura <basmoura@gmail.com>
2014-03-28 12:41:40 -0300

    Insert bin files and change version

--- basmoura.gemspec
+++ basmoura.gemspec
@@ -14,7 +14,7 @@ Gem::Specification.new do |spec|
   spec.license       = "MIT"
 
   spec.files         = `git ls-files -z`.split("\x0")
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.executables   = 'basmoura'
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
@@ -22,4 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'colorize', '~> 0.7.0'
 
   spec.add_development_dependency "bundler", "~> 1.5"
+
+  spec.post_install_message = "This is a simple gem to show my professional informations."\
+    "Type basmoura to see it"
 end
--- lib/basmoura.rb
+++ lib/basmoura.rb
@@ -6,13 +6,17 @@ require "colorize"
 
 module Basmoura
   def self.curriculum
+    clear
     puts "-" * 80
+
+    puts "                            Breno Augusto Santana Moura".yellow
+
     puts "Basic Info".green
     basic_info
     puts ""
 
     puts "Contact Info".green
-    contact_info.each { |k, v| puts "#{k}: #{v}" }
+    contact_info.each { |k, v| puts "#{k.capitalize}: #{v}" }
     puts ""
 
     puts "Qualifications".green
@@ -24,18 +28,19 @@ module Basmoura
     puts "-" * 80
   end
 
+  private
   def self.basic_info
     puts "Brasileiro, Solteiro, #{age_calculate} anos"
   end
 
   def self.contact_info
-    contact = { :phone => "(79) 8835-3428",
+    contact = { :phone => "+55 (79) 8835-3428",
                 :github => "http://github.com/basmoura",
                 :email => "basmoura@gmail.com" }
   end
 
   def self.qualifications
-    puts "- Cursando o sexto período da graduação em sistemas de informação;".green
+    puts "- Cursando o sexto período da graduação em sistemas de informação;"
     puts "- Bons conhecimentos em Ruby on Rails, Java, C#, asp.net, HTML5, CSS3 e Scrum;"
     puts "- Experiência em desenvolvimento de sistemas em Ruby on Rails, asp.net e C#"
   end
@@ -46,11 +51,14 @@ module Basmoura
                   :pt_BR => "Advançado" }
   end
 
-  private
   def self.age_calculate
     today = Date.today
     birthdate = Date.parse("27/09/1988")
     age = today.year - birthdate.year
     age = age - 1 if birthdate.month > today.month
+  end
+
+  def self.clear
+    system('clear')
   end
 end
